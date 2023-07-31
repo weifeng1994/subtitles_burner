57365cb86927812d0cad1d44beb9ef904f7a4914
weifeng1994 <weifeng1994@gmail.com>
2023-07-31 09:47:57 +0800

    Added capability to process audio files

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ You will need to install Docker and Docker Compose
 1. Run `docker-compose up -d`
 2. Go to `http://localhost:8000`. You will be in the landing page of FastAPI
 3. Click on the green tab, and click `Try it out`
-4. Upload the video file under `video_file` and subtitles file (.srt only) under `subtitles_file`
+4. Upload the audio/video file under `media_file` and subtitles file (.srt only) under `subtitles_file`
 5. Click `Execute`. FastAPI will show `LOADING`. Wait for it to disappear.
 6. Click `Download file` link to download your file! The filename is your original filename prepended by `subtitled_`
 
@@ -24,6 +24,8 @@ You will need to install Docker and Docker Compose
 3. Upload your video file and subtitle file (.srt), and click "Run!" button
 4. Go to "Status" tab, and your video with subtitles will be displayed.
 
+# Note: 
+You may now upload an audio file and the app will convert it into an MP4 file using a black image, and burns subtitles into it.
 
 # Issue
 - There is currently a problem with playing the subtitled video
--- src/main.py
+++ src/main.py
@@ -1,22 +1,28 @@
-import tempfile
 import codecs
+import logging
+import tempfile
+from enum import Enum
 
 import uvicorn
 from fastapi import Depends, FastAPI, File, HTTPException, UploadFile
-from fastapi.responses import FileResponse, RedirectResponse
+from fastapi.responses import FileResponse, RedirectResponse, StreamingResponse
 
-from enum import Enum
-from src.burn_subtitles import burn_subtitles, fileHasVideoStream
+from src.burn_subtitles import (burn_subtitles, check_file_stream_type,
+                                convert_audio_to_video)
+
+logging.basicConfig(level=logging.INFO)
 
 app = FastAPI(
     title="Subtitles Burner",
-    description="An application to burn subtitle file into video file"
+    description="An application to burn subtitle file into video file",
 )
+
 
 # Define a class to allow user to select input on Swagger UI
 class OutputType(str, Enum):
     Files = "Files"
     Base64 = "Base64"
+
 
 # It seems that the directory and contents are already being released before the FastAPI request is returned.
 # However we can use Dependency injection in FastAPI, to inject a temporary directory so that we can return a FileResponse from a tempdir
@@ -32,7 +38,7 @@ async def get_temp_dir():
 # Send POST request to localhost:8000/file
 @app.post("/file")
 async def create_file(
-    video_file: UploadFile = File(...),
+    media_file: UploadFile = File(...),
     subtitle_file: UploadFile = File(...),
     output_type: OutputType = OutputType.Files,
     tmpdirname: str = Depends(get_temp_dir),
@@ -50,64 +56,110 @@ async def create_file(
     # Create temporary directory to store generated files, and they will be removed after returning the output
     # with tempfile.TemporaryDirectory() as tmpdirname:
     # Get input video file name
-    ori_video_filename = video_file.filename
+    ori_filename = media_file.filename
+    # Get filename without extension
+    ori_filename_no_ext = ori_filename.split(".")[0]
     # Get video extension
-    video_ext = ori_video_filename.split(".")[-1]
-    video_content_type = video_file.content_type
+    media_ext = ori_filename.split(".")[-1]
+    media_content_type = media_file.content_type
 
     # Define relevant paths
-    input_video_file = f"{tmpdirname}/input_video"
+    input_file = f"{tmpdirname}/input_file"
+    input_converted_file = f"{tmpdirname}/input_converted_file.mp4"
     input_subtitle_file = f"{tmpdirname}/input_subtitles"
-    output_video_file = f"{tmpdirname}/output_video.{video_ext}"
 
     # Read video and subtitles file and write to the respective tempfiles
-    video_buffer = await video_file.read()
+    video_buffer = await media_file.read()
     subtitle_buffer = await subtitle_file.read()
 
-    with open(input_video_file, "wb") as fv:
+    with open(input_file, "wb") as fv:
         fv.write(video_buffer)
 
     with open(input_subtitle_file, "wb") as fs:
         fs.write(subtitle_buffer)
 
+    logging.info("Checking file type ... ")
     # Check if file is a valid video file
-    valid_video_file = fileHasVideoStream(input_video_file)
+    file_stream_type = check_file_stream_type(input_file)
 
     # Raise error if the video file is invalid
-    if not valid_video_file:
+    if not file_stream_type:
         raise HTTPException(
-            status_code=415, detail="The file uploaded is not a valid video file!"
+            status_code=415,
+            detail="The file uploaded is not a valid video or audio file!",
         )
 
-    # Proceed to burn subtitles
-    burn_subtitles_succeed = burn_subtitles(
-        input_video_file, input_subtitle_file, output_video_file
-    )
+    if file_stream_type == "audio":
+        logging.info("File type is audio! Converting to mp4 ... ")
+        # Convert audio file into MP4 by adding a black image
+        convert_audio_file_succeed = convert_audio_to_video(
+            input_file, input_converted_file
+        )
+
+        if not convert_audio_file_succeed:
+            raise HTTPException(
+                status_code=500, detail="Error converting audio file to video file!"
+            )
+
+        output_video_file = f"{tmpdirname}/output_video.mp4"
+
+        logging.info("Burning subtitles ...")
+        # Proceed to burn subtitles as mp4 file
+        burn_subtitles_succeed = burn_subtitles(
+            input_converted_file, input_subtitle_file, output_video_file
+        )
+
+        # Changed media extenstion to mp4 as we are outputing a mp4 file
+        media_ext = "mp4"
+        # Changed media content type to video/mp4 as we are outputing a mp4 file
+        media_content_type = "video/mp4"
+
+    else:
+        output_video_file = f"{tmpdirname}/output_video.{media_ext}"
+
+        logging.info("File type is video! Burning subtitles ...")
+        # Proceed to burn subtitles
+        burn_subtitles_succeed = burn_subtitles(
+            input_file, input_subtitle_file, output_video_file
+        )
 
     if burn_subtitles_succeed:
         if output_type == OutputType.Base64:
-        # After succeeding, return the video file in base64 format
+            # After succeeding, return the video file in base64 format
             with open(output_video_file, "rb") as fo:
                 output_video_file_buffer = fo.read()
-            
 
             output_video_file_base64 = codecs.encode(
                 output_video_file_buffer, "base64"
             ).decode()
             # Return in JS readable format. Remove all \n otherwise video won't be loaded
             return (
-                f"data:{video_content_type};base64,{output_video_file_base64}".replace(
+                f"data:{media_content_type};base64,{output_video_file_base64}".replace(
                     "\n", ""
                 )
             )
         else:
-            return FileResponse(output_video_file, media_type=video_content_type, filename=f"subtitled_{ori_video_filename}")
+
+            def iterfile():
+                CHUNK_SIZE = 1024 * 1024  # = 1MB - adjust the chunk size as desired
+                with open(output_video_file, "rb") as f:
+                    while chunk := f.read(CHUNK_SIZE):
+                        yield chunk
+
+            headers = {
+                "Content-Disposition": f'attachment; filename="subtitled_{ori_filename_no_ext}.{media_ext}"'
+            }
+            return StreamingResponse(
+                iterfile(), headers=headers, media_type="application/octet-stream"
+            )
+            # return FileResponse(output_video_file, media_type=media_content_type, filename=f"subtitled_{ori_filename}")
 
     else:
         # Inform user that burning of subtitle failed if an error occurs
         raise HTTPException(
             status_code=500, detail="Error during hard subtitling of video"
         )
+
 
 @app.get("/", include_in_schema=False)
 def redirect_response():
